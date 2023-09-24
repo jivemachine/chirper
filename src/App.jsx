6d0dfd3dc64f7e3b45570dcdac494eac157372ee
Jeremy <the.jeremy.cobb@gmail.com>
2023-09-24 17:48:02 -0500

    added usestate and use effect to project, added hooks and handlers to set username and chirp

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, effectState } from 'react';
 
 const App = () => {
+
+    // state hooks for username and chirp
+    const [userName, setUserName] = useState('');
+    const [chirp, setChirp] = useState('');
+
+    // handel set username and set chirp funcs
+    const handelUserName = name => {
+        setUserName(name.target.value);
+    };
+    const handelChirp = chirp => {
+        setChirp(chirp.target.value);
+    };
+
     return(
-        <>
-            <h1>lmao hi</h1>
+        <>  
+            {/* add chirp card */}
+            <div className='card m-2' style={{ width: "18rem" }}>
+                <div className='card-body'>
+                <input type="text" className='card-text' placeholder='Username' value={userName} onChange={handelUserName} />
+                    <textarea className='card-text mt-2' placeholder='Your thoughts...' value={chirp} onChange={handelChirp} />
+                    <button className='btn btn-primary mt-2' >Add Chirp!</button>
+                </div>
+            </div>
         </>
     );
 }
