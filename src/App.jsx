e159231b5395cfb31bdb617242e24eeeab12c1b7
Jeremy <the.jeremy.cobb@gmail.com>
2023-09-28 09:26:55 -0500

    added hardcoded chirps

--- src/App.jsx
+++ src/App.jsx
@@ -13,22 +13,36 @@ const App = () => {
     const handelChirp = chirp => {
         setChirp(chirp.target.value);
     };
-  
-    const handleClick = timelineArray => {
-        timelineArray = [];
-        timelineArray.push(userName);
-        timelineArray.push(chirp);
-        console.log(timelineArray);
-    }
 
-    return(
+    return (
         <>  
+            <div className='container'>
             {/* add chirp card */}
             <div className='card m-2' style={{ width: "18rem" }}>
                 <div className='card-body'>
                     <input type="text" className='card-text' placeholder='Username' value={userName} onChange={handelUserName} />
                     <textarea className='card-text mt-2' placeholder='Your thoughts...' value={chirp} onChange={handelChirp} />
-                    <button className='btn btn-primary mt-2' onClick={handleClick}>Add Chirp!</button>
+                    <button className='btn btn-primary mt-2' >Add Chirp!</button>
+                </div>
+            </div>
+            {/* add timeline modal */}
+                <div className='card m-2' style={{ width: "18rem" }}>
+                    <div className='card-body'>
+                        <h5 className="card-title">Spongebob</h5>
+                        <p className="card-text">Is mayonaise an instrument?</p>
+                    </div>
+                </div>
+                <div className='card m-2' style={{ width: "18rem" }}>
+                    <div className='card-body'>
+                        <h5 className="card-title">Patrick</h5>
+                        <p className="card-text">@Squidward I wanna know if horseraddish is an instrument, too!</p>
+                    </div>
+                </div>
+                <div className='card m-2' style={{ width: "18rem" }}>
+                    <div className='card-body'>
+                        <h5 className="card-title">Squidward</h5>
+                        <p className="card-text">Shut the hell up you guys....</p>
+                    </div>
                 </div>
             </div>
         </>
